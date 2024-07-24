c3a95cda63ed7cab3384bf5f824ceb6d9e8e095a
Vishnu tyagi <vishnu.tyagi.mec20@itbhu.ac.in>
2024-07-24 13:29:07 +0530

    error mt dena

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,37 @@
 
 import React from "react";
 import './../styles/App.css';
+import { useState } from "react";
 
 const App = () => {
+  const [task,setTask]=useState("");
+  const [list,setList]=useState([]);
+  // const [changedlist, setChangedlist]=useState([]);
+  const changed=(e)=>{
+    setTask(e.target.value);
+  }
+  const clicked=(e)=>{
+    setList([...list,task]);
+    e.preventDefault();
+  }
+  function deleted(i){
+    const newlist=list.filter((item,index)=> index!==i);
+    setList(newlist);
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <input type="text" onChange={changed}></input>
+        <button onClick={clicked}>Add Todo</button>
+        <div>
+          <ul>
+          {
+            list.map((item,index)=>{
+              return <li>{item} <button onClick={() => deleted(index)}>Delete</button></li>;
+            })
+          }
+          </ul>
+        </div>
     </div>
   )
 }
